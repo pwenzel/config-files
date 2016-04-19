cc3e6bc08a4fd1a5df6dc96ef66c27162a0036a4
Paul Wenzel <paul.wenzel@gokartlabs.com>
2016-04-19 11:35:59 -0500

    Import pandora.fish

---
+++ .config/fish/pandora.fish
@@ -0,0 +1,5 @@
+# Pianobar with media keys support: https://github.com/shayne/PianoKeys
+# Pianobar sans errors: https://github.com/PromyLOPh/pianobar/issues/529
+function pandora
+	pianokeys; and pianobar 2>/dev/null
+end
